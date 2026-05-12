fa1d329b670e9f039a3c81b403a6868137258bd4
elkumah <e.fkumah@gmail.com>
2026-05-12 16:48:16 +0000

    Feat: Configure public subnet resource

--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "main" {
         Name = var.vpc_name
     }   
 }
+# Create a public subnet
+resource "aws_subnet" "public" {
+    vpc_id     = aws_vpc.main.id
+    cidr_block = var.public_subnet_cidr
+    availability_zone = var.availability_zone
+    map_public_ip_on_launch = true
+    tags = {
+        Name = "${var.vpc_name}-PublicSubnet"
+    }
+}
