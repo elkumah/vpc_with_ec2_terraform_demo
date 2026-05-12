e4f5a4ef3e3539eed8d0b2f0613edf980d6351e4
elkumah <e.fkumah@gmail.com>
2026-05-12 17:02:35 +0000

    Feat: Provision IG, Route Table, Associate Route Table, Security Group

--- main.tf
+++ main.tf
@@ -16,3 +16,56 @@ resource "aws_subnet" "public" {
         Name = "${var.vpc_name}-PublicSubnet"
     }
 }
+# Create an Internet Gateway
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.main.id
+    tags = {
+        Name = "${var.vpc_name}-IGW"
+    }
+}
+# Create a route table
+resource "aws_route_table" "public" {                                                                               
+    vpc_id = aws_vpc.main.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.igw.id
+    }
+    tags = {
+        Name = "${var.vpc_name}-PublicRouteTable"
+    }
+}
+# Associate the route table with the public subnet
+resource "aws_route_table_association" "public_assoc" {
+    subnet_id      = aws_subnet.public.id
+    route_table_id = aws_route_table.public.id
+}
+# Create a security group allowing SSH and HTTP access
+resource "aws_security_group" "web_sg" {
+    name        = "${var.vpc_name}-WebSG"
+    description = "Allow SSH and HTTP"
+    vpc_id      = aws_vpc.main.id
+    ingress {
+        description = "Allow SSH"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        description = "Allow HTTP"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        description = "Allow HTTP"
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+        Name = "${var.vpc_name}-WebSG"
+    }
+}   
